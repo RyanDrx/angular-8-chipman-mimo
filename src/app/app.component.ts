6eabb8e5612d2ad89e9251b55259022f3246db47
RyanDrx <ryan.dierickx@gmail.com>
2019-08-12 21:20:47 -0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export interface Estimate {
   minPieces: number;
@@ -26,7 +26,7 @@ const ESTIMATES: Estimate[] = [
     ], overtimeTotal: 825, regularTotal: 725
   },
   {
-    minPieces: 16, maxPieces: 30, workers: [
+    minPieces: 15, maxPieces: 30, workers: [
       { position: 'Supervisor', quantity: 1, hours: 5, regularCost: 0.00, overtimeCost: 0.00 },
       { position: 'Driver', quantity: 1, hours: 5, regularCost: 450.00, overtimeCost: 500.00 },
       { position: 'Installer', quantity: 1, hours: 5, regularCost: 0.00, overtimeCost: 0.00 },
@@ -34,7 +34,7 @@ const ESTIMATES: Estimate[] = [
     ], overtimeTotal: 825, regularTotal: 725
   },
   {
-    minPieces: 31, maxPieces: 60, workers: [
+    minPieces: 30, maxPieces: 60, workers: [
       { position: 'Supervisor', quantity: 1, hours: 6, regularCost: 0.00, overtimeCost: 0.00 },
       { position: 'Driver', quantity: 1, hours: 6, regularCost: 540.00, overtimeCost: 660.00 },
       { position: 'Installer', quantity: 1, hours: 6, regularCost: 0.00, overtimeCost: 0.00 },
@@ -42,7 +42,7 @@ const ESTIMATES: Estimate[] = [
     ], overtimeTotal: 1440, regularTotal: 1200
   },
   {
-    minPieces: 61, maxPieces: 90, workers: [
+    minPieces: 60, maxPieces: 90, workers: [
       { position: 'Supervisor', quantity: 1, hours: 7, regularCost: 0.00, overtimeCost: 0.00 },
       { position: 'Driver', quantity: 1, hours: 7, regularCost: 630.00, overtimeCost: 770.00 },
       { position: 'Installer', quantity: 1, hours: 7, regularCost: 0.00, overtimeCost: 0.00 },
@@ -50,7 +50,7 @@ const ESTIMATES: Estimate[] = [
     ], overtimeTotal: 2135, regularTotal: 1785
   },
   {
-    minPieces: 91, maxPieces: 120, workers: [
+    minPieces: 90, maxPieces: 120, workers: [
       { position: 'Supervisor', quantity: 1, hours: 8, regularCost: 0.00, overtimeCost: 0.00 },
       { position: 'Driver', quantity: 2, hours: 8, regularCost: 1440.00, overtimeCost: 1600.00 },
       { position: 'Installer', quantity: 1, hours: 8, regularCost: 0.00, overtimeCost: 0.00 },
@@ -58,7 +58,7 @@ const ESTIMATES: Estimate[] = [
     ], overtimeTotal: 3160, regularTotal: 2760
   },
   {
-    minPieces: 121, maxPieces: 150, workers: [
+    minPieces: 120, maxPieces: 999, workers: [
       { position: 'Supervisor', quantity: 1, hours: 9, regularCost: 0.00, overtimeCost: 0.00 },
       { position: 'Driver', quantity: 2, hours: 9, regularCost: 1620.00, overtimeCost: 1980.00 },
       { position: 'Installer', quantity: 1, hours: 9, regularCost: 0.00, overtimeCost: 0.00 },
@@ -72,27 +72,34 @@ const ESTIMATES: Estimate[] = [
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   value = 0;
-  estimate: Estimate = null;
-  estimated = false;
+  public estimate: Estimate
+  public workers: Worker[]
+  public estimated = false;
+
+  displayedColumns: string[] = ['position', 'quantity', 'hours', 'regularCost', 'overtimeCost'];
+
+  ngOnInit() {
+    this.estimate = null;
+    this.workers = null;
+  }
+  
 
   sliderChange(event) {
-    const estimate = this.computeCost();
-    console.log(estimate);
+    this.value = event.value;
+    const estimate = this.computeEstimate();
     this.estimate = estimate;
+    this.workers = estimate.workers;
   }
 
-  computeCost() {
-    if (this.value < 1) {
-      this.estimated = false;
-      return null
-    }
-
+  computeEstimate() {
     this.estimated = true;
-    return ESTIMATES.filter(
-      (est: Estimate) =>
-        est.maxPieces <= this.value && est.minPieces > this.value)[0]
+
+    const belowMax = ESTIMATES.filter(
+      (est: Estimate) => this.value <= est.maxPieces);
+
+    return belowMax.filter((est: Estimate) => this.value > est.minPieces)[0];
   }
 
 }
